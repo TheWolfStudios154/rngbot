48ded688ddce96e3d9490c9a891809422dddbe98
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-09-24 05:48:42 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+const discord = require('discord.js');
+var bot = new discord.Client();
+var config = require('./configs/baseconfig');
+var prefix = "."
+bot.login(process.env.token);
+
+// ===Loading commands===
+
+bot.commands = new discord.Collection()
+
+require('fs').readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+// ===Done Loading commands===
+
+
+bot.on('ready', () => {
+  bot.user.setActivity("Hello!")
+    console.log("RNG Bot Online")
+})
+
+bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(prefix)) return;
+  
+  if (cmd) {
+    cmd.run(bot, message, args, discord)
+    console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+  }
+})
