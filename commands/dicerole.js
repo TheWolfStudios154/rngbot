a3a1583200be5f5ecd2e65708cf11f7759749a93
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-09-25 20:24:14 -0400

    Create dicerole.js

---
+++ commands/dicerole.js
@@ -0,0 +1,7 @@
+module.exports.run = (bot, message, args, discord) => {
+ let em = new discord.RichEmbed()
+  .setTitle("🎲 Dice Roll 🎲")
+  .setDescription("Role a Dice!")
+  .addField(Math.floor(Math.random()*6))
+  message.channel.send({embed: em})
+}
