0d4b841820589f8e2a652431d606969ada8d1e55
TheWolfStudios154 <43331514+TheWolfStudios154@users.noreply.github.com>
2018-09-28 06:22:45 -0400

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,14 +1,17 @@
 module.exports.run = (bot, message, args, discord) => {
- let em = new discord.RichEmbed()
-  .setTitle("🎲 Dice Roll 🎲")
-  .setDescription("Role a Dice!")
-  .addField(Math.floor(Math.random()*6))
-  message.channel.send({embed: em})
+    let em = new discord.RichEmbed()
+    .setTitle("🎲 Your dice roll is: 🎲")
+    .setDescription(Math.floor(Math.random() * 6))
+    .setFooter(`${message.author.username}'s dice roll.`)
+    .setColor("GREEN")
+
+    message.channel.send({embed: em})
+
 }
 
 {
     module.exports.help = {
-        name: "rng"
+        name: "roll"
     }
 
 }
